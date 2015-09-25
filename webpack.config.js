681672e0ce97ce9d12cde8f9eb65318761144496
Olivier Blanc <olivier.blanc@bfs.admin.ch>
2015-09-25 17:10:56 +0200

    Css

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
+  module:{
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: path.resolve(ROOT_PATH, 'app')
+    }]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Kanban app'
