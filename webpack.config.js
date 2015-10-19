44bac6192ec537ece901f567fec9711ac2eb420a
Olivier Blanc <olivier.blanc@bfs.admin.ch>
2015-10-19 20:47:09 +0200

    Bundle splitting and versioning

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,25 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
+var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 var common = {
-  entry: path.resolve(ROOT_PATH, 'app'),
+  entry: APP_PATH,
   resolve:{
     extensions: ['','.js', '.jsx']
   },
   output: {
-    path: path.resolve(ROOT_PATH, 'build'),
+    path: BUILD_PATH,
     filename: 'bundle.js'
   },
   module:{
     loaders: [
-      {test: /\.css$/, loaders: ['style', 'css'], include: path.resolve(ROOT_PATH, 'app')},
+      {test: /\.css$/, loaders: ['style', 'css'], include: APP_PATH},
     ]
   },
   plugins: [
@@ -37,7 +40,7 @@ module.exports = merge(common, {
     },
     module:{
       loaders: [
-        {test: /\.js[x]?$/, loaders: ['react-hot', 'babel'], include: path.resolve(ROOT_PATH, 'app')}
+        {test: /\.js[x]?$/, loaders: ['react-hot', 'babel'], include: APP_PATH}
       ]
     },
     plugins: [new webpack.HotModuleReplacementPlugin()]  
@@ -46,17 +49,24 @@ module.exports = merge(common, {
 
 if (TARGET == 'build') {
 module.exports = merge(common, {
-    devtool : 'source-map',
-    devServer: {
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true
+    entry: {
+      app: APP_PATH,
+      vendor: Object.keys(pkg.dependencies)
     },
+    output: {
+      path: BUILD_PATH,
+      filename: '[name].[chunkhash].js'
+    },
+    devtool : 'source-map',
     module:{
       loaders: [
-        {test: /\.js[x]?$/, loaders: ['babel'], include: path.resolve(ROOT_PATH, 'app')}
+        {test: /\.js[x]?$/, loaders: ['babel'], include: APP_PATH}
       ]
-    }
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin('vendor', '[name].[chunkhash].js'),
+      new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
+      new webpack.optimize.UglifyJsPlugin({ compress: {warnings: false}})
+    ]
   });  
 }
